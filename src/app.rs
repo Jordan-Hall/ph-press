f796ee85a40b8729e03d12b33e382fe941c02206
Jordan Upton <jordan@predatorhunters.co.uk>
2026-06-22 22:04:13 +0100

    feat(compliance): public /complaints policy + /corrections log pages

--- src/app.rs
+++ src/app.rs
@@ -8,8 +8,9 @@ use crate::assets::{FAVICON, PH_LOGO};
 use crate::components::{ClosingCta, SiteFooter};
 use crate::icons::svg;
 use crate::pages::{
-    About, Article, ComplaintPage, Contact, Database, Desk, DeskForgot, DeskPreview, DeskReset,
-    Home, News, NotFound, Podcast, Privacy, Standards, Team, Watch, WriteArticle,
+    About, Article, ComplaintPage, ComplaintsPolicy, Contact, CorrectionsLog, Database, Desk,
+    DeskForgot, DeskPreview, DeskReset, Home, News, NotFound, Podcast, Privacy, Standards, Team,
+    Watch, WriteArticle,
 };
 
 #[derive(Routable, Clone, PartialEq)]
@@ -45,8 +46,12 @@ pub enum Route {
     Team {},
     #[route("/standards")]
     Standards {},
+    #[route("/complaints")]
+    ComplaintsPolicy {},
     #[route("/complaints/:slug")]
     ComplaintPage { slug: String },
+    #[route("/corrections")]
+    CorrectionsLog {},
     #[route("/contact")]
     Contact {},
     #[route("/privacy")]
